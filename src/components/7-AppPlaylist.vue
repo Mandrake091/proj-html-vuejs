5717b1dcee6857dff553121d86cd69e78d8b795e
Paolo Cannistraro <wasdgamer91@gmail.com>
2022-05-19 12:59:50 +0200

    added hover in playlist cards

--- src/components/7-AppPlaylist.vue
+++ src/components/7-AppPlaylist.vue
@@ -17,47 +17,44 @@
           class="row justify-content-between align-items-center text-center mb-5"
         >
           <div
-            id="img2"
             class="col-12 col-md-4"
             data-aos="fade-right"
             data-aos-offset="200"
             data-aos-delay="100"
             data-aos-duration="1000"
           >
-            <img class="" src="../assets/images/blog4-2x.jpg" alt="" />
+            <div id="img1" class="hover-background"></div>
 
             <div class="mt-5">
-              <h2>Thighs & glute workout</h2>
+              <h3>Thighs & glute workout</h3>
               <p>Increase your mobility</p>
             </div>
           </div>
 
           <div
-            id="img3"
             class="col-12 col-md-4"
             data-aos="fade-up"
             data-aos-offset="200"
             data-aos-delay="100"
             data-aos-duration="1000"
           >
-            <img class="" src="../assets/images/blog1-2x.jpg" alt="" />
+            <div id="img2" class="hover-background"></div>
 
             <div class="mt-5">
-              <h2>Lift, firm & perk up</h2>
+              <h3>Lift, firm & perk up</h3>
               <p>Fell young again</p>
             </div>
           </div>
           <div
-            id="img4"
             class="col-12 col-md-4"
-            data-aos="fade-rigth"
-            data-aos-offset="200"
+            data-aos="fade-right"
+            data-aos-offset=""
             data-aos-delay="100"
             data-aos-duration="1000"
           >
-            <img class="" src="../assets/images/blog3-2x.jpg" alt="" />
+            <div id="img3" class="hover-background"></div>
             <div class="mt-5 d-sm-none d-md-block">
-              <h2>Slim & trim trim your waist</h2>
+              <h3>Slim & trim trim your waist</h3>
               <p>Shed those extra pounds</p>
             </div>
           </div>
@@ -90,38 +87,27 @@ export default {
   width: 80%;
   margin: 0 auto;
 }
-
-#img2 {
+.col-12{
+  height: 300px;
+}
+.hover-background {
+  width: 100%;
   height: 220px;
   filter: drop-shadow(5px 5px 10px rgba(0, 0, 0, 0.481));
-}
-#img2 > img {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  object-position: center center;
-}
-#img3 {
-  height: 220px;
-  filter: drop-shadow(5px 5px 10px rgba(0, 0, 0, 0.481));
-}
-#img3 > img {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  object-position: center center;
-}
-#img4 {
-  height: 220px;
-  filter: drop-shadow(5px 5px 10px rgba(0, 0, 0, 0.481));
-}
-#img4 > img {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  object-position: center center;
+  background-size: cover;
+  background-position: center center;
+  transition: 1s ease;
+  &#img1 {
+    background-image: url("../assets/images/blog4-2x.jpg");
+  }
+  &#img2 {
+    background-image: url("../assets/images/blog1-2x.jpg");
+  }
+  &#img3 {
+    background-image: url("../assets/images/blog3-2x.jpg");
+  }
+  &:hover {
+    box-shadow: inset 0px -50px 100px rgba(149, 149, 255, 0.549);
+  }
 }
 </style>
